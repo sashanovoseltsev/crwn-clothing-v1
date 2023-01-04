7d8995f9c27ba26a0bbecc5e446e16ff5fb3240c
onovosel <oleksandr.novoseltsev@avid.com>
2023-01-05 00:10:20 +0200

    preparation + sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import "./categories.styles.scss";
+
 const App = () => {
-  return <div>Hello World!</div>;
+  const categories = [
+    {
+      id: "1",
+      title: "Hats",
+    },
+    {
+      id: "2",
+      title: "Jackets",
+    },
+    {
+      id: "3",
+      title: "Sneakers",
+    },
+    {
+      id: "4",
+      title: "Womens",
+    },
+    {
+      id: "5",
+      title: "Mens",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div class="background-image " />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
